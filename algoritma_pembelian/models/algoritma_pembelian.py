4433aa4df06abe167118b06a62601c3cfc55025e
SeRyuu25 <18220025@std.stei.itb.ac.id>
2023-06-21 16:28:39 +0700

    Update ir.actions.server implementation

--- algoritma_pembelian/models/algoritma_pembelian.py
+++ algoritma_pembelian/models/algoritma_pembelian.py
@@ -6,6 +6,21 @@ class algoritma_pembelian(models.Model): # pembuatan tabel baru
     # nama tabelnya
     _name = 'algoritma.pembelian'
 
+    def show_tree_view(self):
+        tree_view_id = self.env['ir.model.data']._xmlid_to_res_id('algoritma_pembelian.algoritma_pembelian_tree_view_id')
+        form_view_id = self.env['ir.model.data']._xmlid_to_res_id('algoritma_pembelian.algoritma_pembelian_form_view_id')
+        domain = [('status', '=', 'draft')]
+        result = {
+            'name' : 'Pembelian B',
+            'type' : 'ir.actions.act_window',
+            'views' : [[tree_view_id, 'tree'], [form_view_id, 'form']],
+            'target' : 'current',
+            'res_model' : 'algoritma.pembelian',
+            'domain' : domain,
+            'limit' : 40
+        }
+        return result
+
     # Pembuatan Error Message (Raise)
     @api.model
     def create(self, values):
@@ -31,11 +46,12 @@ class algoritma_pembelian(models.Model): # pembuatan tabel baru
         self.status = 'draft'
 
     def func_to_approve(self):
-        self.status = 'to_approve'
-        # Code untuk run sequence yang sudah dibuat
-        if self.name == 'New':
-            seq = self.env['ir.sequence'].next_by_code('algoritma.pembelian') or 'New'
-            self.name = seq
+        for line in self:
+            line.status = 'to_approve'
+            # Code untuk run sequence yang sudah dibuat
+            if line.name == 'New':
+                seq = line.env['ir.sequence'].next_by_code('algoritma.pembelian') or 'New'
+                line.name = seq
 
     def func_approved(self):
         self.status = 'approved'
@@ -98,7 +114,7 @@ class algoritma_pembelian_report_wizard(models.TransientModel):
 
     name = fields.Char(string="Name")
     periode_awal = fields.Date(string="Periode Awal")
-    periode_akhir = fields.Date(strin="Periode Akhir")
+    periode_akhir = fields.Date(string="Periode Akhir")
 
 # Class untuk inheritance model
 class product_template(models.Model):
