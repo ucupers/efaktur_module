61a8770e441a66ab70a9c92fdb1f89a187904aa7
FlitzyBlue332 <raihanaulia332@gmail.com>
2023-07-12 15:53:46 +0700

    Update efaktur_controller.py
    Added quotation marks before and after each element

--- efaktur/controller/efaktur_controller.py
+++ efaktur/controller/efaktur_controller.py
@@ -30,7 +30,7 @@ class ReportCSVEfakturController(http.Controller):
 
         # Prepare output variable to store content
         output = io.StringIO()
-        csv_writer = csv.writer(output, delimiter=',')
+        csv_writer = csv.writer(output, delimiter=',', lineterminator='\n',quotechar = "'")
         
         # write header
         csv_writer.writerow(FK_HEADER_FAKTUR_KELUAR)
@@ -50,47 +50,48 @@ class ReportCSVEfakturController(http.Controller):
 
                 # taken from efaktur module, aight temporary spam kopas
                 # FK line
-                datafk['FK'] = 'FK'
-                datafk['KD_JENIS_TRANSAKSI'] = move.l10n_id_tax_number[0:2] or 0
-                datafk['FG_PENGGANTI'] = move.l10n_id_tax_number[2:3] or 0
-                datafk['NOMOR_FAKTUR'] =  move.l10n_id_tax_number[3:] or 0
-                datafk['MASA_PAJAK'] = move.invoice_date.month
-                datafk['TAHUN_PAJAK'] = move.invoice_date.year
-                datafk['TANGGAL_FAKTUR'] = '{0}/{1}/{2}'.format(move.invoice_date.day, move.invoice_date.month, move.invoice_date.year)
-                datafk['NPWP'] = move.partner_id.vat
-                datafk['NAMA'] =  move.partner_id.name if datafk['NPWP'] == '000000000000000' else move.partner_id.l10n_id_tax_name or move.partner_id.name
-                datafk['ALAMAT_LENGKAP'] = move.partner_id.contact_address.replace('\n', '') if datafk['NPWP'] == '000000000000000' else move.partner_id.l10n_id_tax_address or move.partner_id.street
-                datafk['JUMLAH_DPP'] = int(float_round(move.amount_untaxed, 0)) # currency rounded to the unit
-                datafk['JUMLAH_PPN'] = int(float_round(move.amount_tax, 0, rounding_method="DOWN"))  # tax amount ALWAYS rounded down
-                datafk['JUMLAH_PPNBM'] = 0
-                datafk['ID_KETERANGAN_TAMBAHAN'] = '1' if move.l10n_id_kode_transaksi == '07' else ''
-                datafk['FG_UANG_MUKA'] = 0
-                datafk['UANG_MUKA_DPP'] = 0
-                datafk['UANG_MUKA_PPN'] = 0
-                datafk['UANG_MUKA_PPNBM'] = 0
-                datafk['REFERENSI'] = ''
-
+                datafk['FK'] = '"FK"'
+                datafk['KD_JENIS_TRANSAKSI'] = '"' + str(move.l10n_id_tax_number[0:2] or 0) + '"'
+                datafk['FG_PENGGANTI'] = '"' + str(move.l10n_id_tax_number[2:3] or 0) + '"'
+                datafk['NOMOR_FAKTUR'] = '"' + str(move.l10n_id_tax_number[3:] or 0) + '"'
+                datafk['MASA_PAJAK'] = '"' + str(move.invoice_date.month) + '"'
+                datafk['TAHUN_PAJAK'] = '"' + str(move.invoice_date.year) + '"'
+                datafk['TANGGAL_FAKTUR'] = '"' + str('{0}/{1}/{2}'.format(move.invoice_date.day, move.invoice_date.month, move.invoice_date.year)) + '"'
+                datafk['NPWP'] = '"' + str(move.partner_id.vat) + '"'
+                datafk['NAMA'] =  '"' + str(move.partner_id.name if datafk['NPWP'] == '000000000000000' else move.partner_id.l10n_id_tax_name or move.partner_id.name) + '"'
+                datafk['ALAMAT_LENGKAP'] = '"' + str(move.partner_id.contact_address.replace('\n', '') if datafk['NPWP'] == '000000000000000' else move.partner_id.l10n_id_tax_address or move.partner_id.street) + '"'
+                datafk['JUMLAH_DPP'] = '"' + str(int(float_round(move.amount_untaxed, 0))) + '"' # currency rounded to the unit
+                datafk['JUMLAH_PPN'] = '"' + str(int(float_round(move.amount_tax, 0, rounding_method="DOWN"))) + '"'  # tax amount ALWAYS rounded down
+                datafk['JUMLAH_PPNBM'] = '"0"'
+                datafk['ID_KETERANGAN_TAMBAHAN'] = '"' + str('1' if move.l10n_id_kode_transaksi == '07' else '') + '"'
+                datafk['FG_UANG_MUKA'] = '"0"'
+                datafk['UANG_MUKA_DPP'] = '"0"'
+                datafk['UANG_MUKA_PPN'] ='"0"'
+                datafk['UANG_MUKA_PPNBM'] = '"0"'
+                datafk['REFERENSI'] = '""'
+                
                 # LT Line
                 company_id = move.company_id.id
                 current_company = request.env['res.company'].search([('id', '=', str(company_id))])
                 
-                datalt['LT'] = 'FAPR'
-                datalt['NPWP'] = current_company.vat
-                datalt['NAMA'] = current_company.name
-                datalt['JALAN'] = current_company.street
-                datalt['BLOK'] = ''
-                datalt['NOMOR'] = ''
-                datalt['RT'] = ''
-                datalt['RW'] = ''
-                datalt['KECAMATAN'] = ''
-                datalt['KELURAHAN'] = ''
-                datalt['KABUPATEN'] = current_company.city
-                datalt['PROPINSI'] = current_company.state_id.name
-                datalt['KODE_POS'] = current_company.zip
-                datalt['NOMOR_TELEPON'] = current_company.phone
+                datalt['LT'] = '"FAPR"'
+                datalt['NPWP'] = '"' + str(current_company.vat) + '"'
+                datalt['NAMA'] = '"' + str(current_company.name) + '"'
+                datalt['JALAN'] = '"' + str(current_company.street) + '"'
+                datalt['BLOK'] = '""'
+                datalt['NOMOR'] = '""'
+                datalt['RT'] = '""'
+                datalt['RW'] = '""'
+                datalt['KECAMATAN'] = '""'
+                datalt['KELURAHAN'] = '""'
+                datalt['KABUPATEN'] = '"' + str(current_company.city) + '"'
+                datalt['PROPINSI'] = '"' + str(current_company.state_id.name) + '"'
+                datalt['KODE_POS'] = '"' + str(current_company.zip) + '"'
+                datalt['NOMOR_TELEPON'] = '"' + str(current_company.phone) + '"'
                 
                 csv_writer.writerow(datafk.values())
                 csv_writer.writerow(datalt.values())
+                
                 # OF Lines
                 for line in move.line_ids.filtered(lambda l: l.display_type == 'product'):
                     decimal_places = current_company.currency_id.decimal_places
@@ -98,30 +99,18 @@ class ReportCSVEfakturController(http.Controller):
                     total_price = line.quantity * line.price_unit
 
                     dataof = {}
-                    dataof['OF'] = 'OF'
-                    dataof['KODE_OBJEK'] = line.product_id.default_code or ''
-                    dataof['NAMA'] = line.product_id.name or ''
-                    dataof['HARGA_SATUAN'] = float_repr(float_round(line.price_unit, decimal_places), decimal_places)
-                    dataof['JUMLAH_BARANG'] = line.quantity
-                    dataof['HARGA_TOTAL'] = float_repr(float_round(total_price, decimal_places), decimal_places)
-                    dataof['DISKON'] = line.discount
-                    dataof['DPP'] = float_round(line.price_subtotal, decimal_places)
-                    dataof['PPN'] = float_round(line.price_subtotal - line.price_subtotal, decimal_places)
-                    dataof['TARIF_PPNBM'] = 0
-                    dataof['PPNBM'] = 0
+                    dataof['OF'] = '"OF"'
+                    dataof['KODE_OBJEK'] = '"' + str(line.product_id.default_code or '') + '"'
+                    dataof['NAMA'] = '"' + str(line.product_id.name or '') + '"'
+                    dataof['HARGA_SATUAN'] = '"' + str(float_repr(float_round(line.price_unit, decimal_places), decimal_places)) + '"'
+                    dataof['JUMLAH_BARANG'] = '"' + str(line.quantity) + '"'
+                    dataof['HARGA_TOTAL'] = '"' + str(float_repr(float_round(total_price, decimal_places), decimal_places)) + '"'
+                    dataof['DISKON'] = '"' + str(line.discount) + '"'
+                    dataof['DPP'] = '"' + str(float_round(line.price_subtotal, decimal_places)) + '"'
+                    dataof['PPN'] = '"' + str(float_round(line.price_total - line.price_subtotal, decimal_places)) + '"'
+                    dataof['TARIF_PPNBM'] = '"0"'
+                    dataof['PPNBM'] = '"0"'
                     csv_writer.writerow(dataof.values())
-
-                
-
-
-
-
-
-                
-                # for line in move.line_ids.filtered(lambda l: l.display_type == 'product'):
-                #     # Products
-                #     datastring = ['OF']
-                #     csv_writer.writerow(datastring)
         
         output.seek(0)
         response.stream.write(output.read())
